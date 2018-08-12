e165135d0ba80f47ec7439838632eddd0268e248
rehellinen <912377791@qq.com>
2018-08-12 22:48:46 +0800

    电费组件的完成

---
+++ src/base/electricity/electricity.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+    div.elec-container
+      img(src="__IMAGE__/theme/personal@backElec.png")
+      div.elec-detail
+        div.surplus
+          text.surplus-text 剩余电量 (度)
+          text.surplus-num {{surplus}}
+        div.line
+        div.surplus
+          text.surplus-text 近三天用电 (度)
+          text.surplus-num {{threeDays}}
+</template>
+
+<script>
+  export default {
+    props: {
+      threeDays: {
+        type: Number,
+        default: 0
+      },
+      surplus: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
+
+<style scoped lang="sass" rel="stylesheet/sass">
+  @import "~css/base"
+
+  .elec-container
+    background-color: white
+    width: 750rpx
+    height: 185rpx
+    border-radius: 8px
+
+    img
+      width: 750rpx
+      height: 185rpx
+      position: absolute
+
+  .elec-detail
+    position: absolute
+    text-align: center
+    width: 98%
+    display: flex
+    flex-direction: row
+    height: 185rpx
+
+  .surplus
+    width: 49%
+    text-align: center
+    display: flex
+    flex-direction: column
+
+    .surplus-text
+      font-size: $smaller-font-size
+      z-index: 40
+      color: #000
+      margin-top: 30rpx
+
+    .surplus-num
+      font-size: $vast-font-size
+      z-index: 40
+      color: #000
+      margin-top: 20rpx
+
+  .line
+    background-color: $light-font-color
+    width: 1.5px
+    height: 80%
+    margin-left: 49.5%
+    margin-top: 2.5%
+    z-index: 80
+    position: absolute
+</style>
--- src/pages/personal/personal.vue
+++ src/pages/personal/personal.vue
@@ -7,27 +7,53 @@
       div.icon-container
         img(src="__IMAGE__/icon/edit.png")
       // 个人信息栏
+
+      // 电费栏
+      electricity(:threeDays="threeDays", :surplus="surplus")
+      // 电费栏
 </template>
 
 <script>
   import MyLoading from 'base/my-loading/my-loading'
   import TopImage from 'base/top-image/top-image'
+  import Electricity from 'base/electricity/electricity'
 
   export default {
     data () {
       return {
         showLoading: false,
         avatar: '__IMAGE__/theme/personal@avatar.jpg',
-        name: 'tuntematon'
+        name: 'tuntematon',
+        threeDays: 0,
+        surplus: 0
       }
     },
     components: {
       MyLoading,
-      TopImage
+      TopImage,
+      Electricity
     }
   }
 </script>
 
 <style scoped lang="sass" rel="stylesheet/sass">
-
+  @import "~css/base"
+  .icon-container
+    position: absolute
+    width: 80rpx
+    height: 80rpx
+    top: 30rpx
+    right: 20rpx
+    background-color: white
+    border-radius: 50px
+    opacity: 0.8
+    display: flex
+    justify-content: center
+    align-items: center
+    z-index: 100
+    img
+      width: 40rpx
+      height: 40rpx
+  .personal-container
+    background-color: $background-color
 </style>
