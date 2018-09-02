9e93300487638aebfa8aeabcd2c9021d06c86265
rehellinen <912377791@qq.com>
2018-09-02 09:41:48 +0800

    商品详情样式优化

--- src/components/goods-desc/goods-desc.vue
+++ src/components/goods-desc/goods-desc.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
   div.card.desc-container
+    div.goods-icon-container
+      img(src="__IMAGE__/icon/goods-detail.png")
+      p 商品详情：
     p.desc(v-if="desc") {{desc}}
     p.no-goods-desc(v-else) 暂无商品介绍
 </template>
@@ -21,6 +24,18 @@
     text-align: center
     margin: 30rpx 0
     font-size: $small-font-size
+  .goods-icon-container
+    display: flex
+    margin: 0 20rpx
+    padding: 20rpx 0 15rpx 0
+    align-items: center
+    border-bottom: 1px solid $lighter-font-color
+    img
+      width: 35rpx
+      height: 35rpx
+    p
+      font-size: $small-font-size
+      margin-left: 15rpx
   .desc
     padding: 20rpx 20rpx
     font-size: $small-font-size
